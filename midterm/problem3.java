e933da02238a8a2c670a7d2fc636ce39fb47035b
uyrance <uyrance@hotmal.com>
2024-03-22 16:52:13 -0700

    midterm

--- midterm/problem3.java
+++ midterm/problem3.java
@@ -7,17 +7,17 @@ public class problem3 {
     
     public boolean verifyPreorder(int[] preorder) {
         Stack<Integer> stack = new Stack<>();
-        // 初始化最小值为最小整数
+    
         int low = Integer.MIN_VALUE;
         for (int v : preorder) {
-            // 如果遇到的值小于栈中的任何值，则不是有效的前序遍历
+        
             if (v < low) return false;
-            // 如果当前值大于栈顶元素，说明我们正在遍历的是右子树
+      
             while (!stack.isEmpty() && v > stack.peek()) {
-                // 更新我们在当前右子树中应有的最小值
+          
                 low = stack.pop();
             }
-            // 将当前值压入栈中
+      
             stack.push(v);
         }
         return true;
